90059b1d5ab7fb2c9db55b2bdef45575f02c484e
houdejun214 <houdejun214@gmail.com>
2014-02-18 16:08:59 +0800

    find images from SEO meta properties.

--- src/main/java/org/real/html/boilerpipe/HtmlContentImageExtractor.java
+++ src/main/java/org/real/html/boilerpipe/HtmlContentImageExtractor.java
@@ -2,13 +2,16 @@ package org.real.html.boilerpipe;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -18,6 +21,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.real.html.boilerpipe.util.URLCanonicalizer;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -29,7 +33,6 @@ import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
 public class HtmlContentImageExtractor {
 	
 	private static Set<String> ignoreFilesExtends = new HashSet<String>(Arrays.asList("gif","png"));
-	private static Set<String> imageFilesExtends = new HashSet<String>(Arrays.asList("jpg"));
 	
 	private CloseableHttpClient client = HttpClients.createDefault();
 
@@ -37,43 +40,81 @@ public class HtmlContentImageExtractor {
 	
 	private HtmlContentExtractor hh = HtmlContentExtractor.newExtractingInstance();
 	
-	public List<String> getImages(String url) throws IOException, BoilerpipeProcessingException, SAXException{
+	public Collection<String> getImages(String url) throws IOException, BoilerpipeProcessingException, SAXException{
+		String content = downloadWebContent(url);
+		// parser document
+		Collection<String> imgLinks = getImagesByContent(content,url);
+		return imgLinks;
+	}
+	
+	private String downloadWebContent(String url) throws ParseException, IOException{
 		HttpGet get = new HttpGet(url);
 		get.addHeader("Connection", "close");
 		get.addHeader("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.1; pl; rv:1.9.1) Gecko/20090624 Firefox/3.5 (.NET CLR 3.5.30729)");
-
 		CloseableHttpResponse response = client.execute(get);
 		try {
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode != HttpStatus.SC_OK) {
+				if (statusCode != HttpStatus.SC_NOT_FOUND) {
+					if (statusCode == HttpStatus.SC_MOVED_PERMANENTLY || statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
+						Header sheader = response.getFirstHeader("Location");
+						if (sheader != null) {
+							String redictUrl = sheader.getValue();
+							redictUrl = URLCanonicalizer.getCanonicalURL(redictUrl, url);
+							return downloadWebContent(redictUrl);
+						} 
+					}
+				}
+			}
 			HttpEntity entity = response.getEntity();
 			String content = EntityUtils.toString(entity);
-			// parser document
-			List<String> imgLinks = getImagesByContent(content,url);
-			return imgLinks;
+			return content;
 		} finally {
 			response.close();
 		}
 	}
 	
-	public List<String> getImagesByContent(String content,String baseUrl) throws IOException, BoilerpipeProcessingException, SAXException{
-		List<String> imgLinks;
+	public Collection<String> getImagesByContent(String content,String baseUrl) throws IOException, BoilerpipeProcessingException, SAXException{
+		Set<String> imgLinks = new LinkedHashSet<String>();
 		// parser document
 		TextDocument textDoc = new BoilerpipeSAXInput(new InputSource(new StringReader(content))).getTextDocument();
 		extractor.process(textDoc);
 		InputSource htmlDoc = new InputSource(new StringReader(content));
 		String mainContent = hh.process(textDoc, htmlDoc);
 		
-		Document doc = Jsoup.parse(mainContent,baseUrl);
-		Elements imgs = doc.select("img");
-		imgLinks = new ArrayList<String>();
-		if(imgs != null && imgs.size()>0){
-			for(Element img:imgs){
-				String src = img.absUrl("src");
+		// find images from SEO meta properties.
+		Document doc = Jsoup.parse(content,baseUrl);
+		Element imageProp = doc.select("meta[property=og:image]").first();
+		if(imageProp!=null){
+			String src = imageProp.attr("content");
+			if(src!=null && !src.equals("")){
 				String ext = getExtension(src);
 				if(!ignoreFilesExtends.contains(ext)){
 					imgLinks.add(src);
 				}
 			}
 		}
+		
+		// find images from main content panel
+		Document mainContentDoc = Jsoup.parse(mainContent,baseUrl);
+		Elements imgs = mainContentDoc.select("img");
+		if(imgs != null && imgs.size()>0){
+			for(Element img:imgs){
+				String src = img.absUrl("src");
+				if(src==null || src.equals("")){
+					// for some lazy-load images
+					src = img.absUrl("data-original");
+				}
+				if(src!=null && !src.equals("")){
+					String ext = getExtension(src);
+					if(!ignoreFilesExtends.contains(ext)){
+						imgLinks.add(src);
+					}
+				}
+			}
+		}
+		
+		
 		return imgLinks;
 	}
 	
--- src/test/java/org/real/html/boilerpipe/HtmlContentImageExtractorTest.java
+++ src/test/java/org/real/html/boilerpipe/HtmlContentImageExtractorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 import junit.framework.Assert;
@@ -53,11 +54,61 @@ public class HtmlContentImageExtractorTest {
 				"http://blogserius.blogspot.sg/2012/08/serius-cool-alatan-perkemahan-4-dalam-1.html",
 				"http://blogserius.blogspot.sg/2013/07/serius-cool-ferra-tower-menara-dengan.html",
 				"http://www.tiffinbiru.com/2010/12/skodeng-dapur-mat-gebuada-brann.html",
+				
+				"http://www.straitstimes.com/breaking-news/singapore/story/median-household-income-rose-7870-last-year-inequality-down-20140218",
+				"http://www.straitstimes.com/breaking-news/singapore/story/be-better-friend-and-put-your-mobile-phone-friend-mode-20140218",
+				"http://www.straitstimes.com/breaking-news/singapore/story/manpower-miinistry-step-safety-enforcement-construction-industry-20140",
+				"http://stcommunities.straitstimes.com/movies/2014/02/18/queen-hosts-british-film-royalty-palace",
+				"http://stcommunities.straitstimes.com/show/2014/02/18/miami-museum-visitor-charged-breaking-126m-ai-weiwei-vase",
+				"http://www.pinterest.com/pin/378654281139653100/",
+				"http://www.pinterest.com/pin/378654281141264757/",
+				"http://www.sgcarmart.com/used_cars/info.php?ID=387061&DL=1000",
+				"http://www.sgcarmart.com/new_cars/newcars_overview.php?CarCode=10732",
+				"http://www.scmp.com/news/china/article/1430102/i-thought-it-was-home-depot-pot-painter-charged-smashing-museums-us1m-ai",
+				"http://www.scmp.com/news/hong-kong/article/1429896/hong-kong-children-happier-less-able-handle-adversity-survey-finds",
+				"http://tech.sina.com.cn/mobile/n/2014-02-18/08049170370.shtml",
+				"http://item.taobao.com/item.htm?id=37022645349&ali_trackid=2:mm_15890324_2192376_11153435,0:1392705944_6k3_516481380&clk1=eb4f7d28cd2a3882f56db91eee0a36d6&spm=a3300.2168033.5636241.5.IRO46g",
+				"http://detail.tmall.com/item.htm?id=20117869226",
+				"http://entertainment.xin.msn.com/en/celebrity/buzz/asia/flumpool-treats-fans-to-special-showcase-in-singapore",
+				"http://entertainment.xin.msn.com/en/celebrity/buzz/hollywood/stars-on-the-catwalk-11",
+				"http://news.insing.com/feature/a-case-of-molest-on-the-mrt/id-fe393101",
+				"http://stylplus.insing.com/feature/alice-olivia-to-open-in-singapore/id-ea393101",
+				"http://movies.insing.com/gallery/12-movie-stars-then-and-now/id-4c2e3101/",
+				"http://stylplus.insing.com/gallery/stop-habits-lose-weight/id-54400400/slide",
+				"http://stylplus.insing.com/feature/hair-treatments-from-the-supermarket/id-84643f00",
+				"http://slide.ent.sina.com.cn/star/slide_4_704_75377.html#p=1",
+				"http://ent.sina.com.cn/s/h/2014-02-18/07474098254.shtml",
+				"http://slide.ent.sina.com.cn/star/h/slide_4_704_75375.html#p=1",
+				"http://slide.fashion.sina.com.cn/s/slide_24_55010_42042.html#p=1",
+				"http://fashion.sina.com.cn/b/ha/2014-02-18/071735466.shtml",
+				"http://www.meilishuo.com/share/item/2379882175?d_r=0.1.1.1",
+				"http://www.clozette.co/community/browse/e1b4886a8cd34addbff75b8a1595ddac/stylespresso",
+				"http://www.glamasia.com/2014/02/18/trend-alert-tea-length-skirts/",
+				"http://clozette.glam.jp/community/browse/5e995821db274dc4a05b18bf68b53350/GlamClozette",
 		};
 		for(String url:urls){
-			List<String> images = extractor.getImages(url);
-			assertTrue(url+" can not be extract",images.size()>0);
-			System.out.println(StringUtils.join(images, ","));
+			Collection<String> images = extractor.getImages(url);
+			if(images.size()<=0){
+				System.out.println("["+url+"] can not be extract");
+			}else{
+				System.out.println(StringUtils.join(images, ","));
+			}
+		}
+	}
+	
+	@Test
+	public void testCase1() throws IOException, BoilerpipeProcessingException, SAXException {
+		String[] urls = new String[]{
+				"http://www.scmp.com/news/china/article/1430102/i-thought-it-was-home-depot-pot-painter-charged-smashing-museums-us1m-ai",
+				"http://www.scmp.com/news/hong-kong/article/1429896/hong-kong-children-happier-less-able-handle-adversity-survey-finds",
+		};
+		for(String url:urls){
+			Collection<String> images = extractor.getImages(url);
+			if(images.size()<=0){
+				System.out.println("["+url+"] can not be extract");
+			}else{
+				System.out.println(StringUtils.join(images, ","));
+			}
 		}
 	}
 }
